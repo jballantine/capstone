9969a7b827a198dbf3bf37dbab9fc568442ca740
EC2 Default User <ec2-user@ip-172-31-89-180.ec2.internal>
2022-09-11 17:03:09 +0000

    Push image to docker step

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Takes 3 args: tag name, username and password
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=$2/$1
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login -u $2 -p $3
+docker tag $1 $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
