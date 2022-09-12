fa93264f7bc0809f2a8d832e4fb82927c2326b3b
EC2 Default User <ec2-user@ip-172-31-83-110.ec2.internal>
2022-09-12 12:28:54 +0000

    Deploy blue/green containers and run blue/green services

--- installKube.sh
+++ installKube.sh
@@ -2,5 +2,5 @@
 # Installs kubectl to allow jenkins to communicate with Kubernetes API server
 
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+chmod 777 ./kubectl
 mkdir -p $1/bin && cp ./kubectl $1/bin/kubectl && export PATH=$2:$1/bin
